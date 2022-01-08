849490abcede6eaa30b26c88dfe4dffc6ccc4ba4
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-08 13:07:19 +0530

    Adding uc1.cs File

---
+++ Uc1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_GeomatryProblems
+{
+    public class Uc1 : Test
+    {
+		public override void Length()
+		{
+			double length = xLength + yLength;
+			Console.WriteLine("X and Y lien length is :- " + length);
+		}
+	}
+}
