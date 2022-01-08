ea66779c8fe82e9415b0c31f0f6286429176a69e
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-08 13:10:33 +0530

    Adding uc3.cs File

---
+++ Uc3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_GeomatryProblems
+{
+    internal class Uc3 : Test
+    {
+		public override void Length()
+		{
+
+			Console.WriteLine("X line length is " + xLength);
+			Console.WriteLine("Y line length is " + yLength);
+
+			if (xLength == yLength)
+				Console.WriteLine("Two lines are equal");
+		else if (xLength < yLength)
+				Console.WriteLine("Y line is greater than X line");
+		else if (yLength < xLength)
+				Console.WriteLine("X line is greater than y line");
+
+		}
+	}
+}
