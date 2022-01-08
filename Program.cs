b0a7847c16007c0e77b3a9e6d6cc95bca535642a
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-08 13:13:32 +0530

    Adding Program.cs File

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_GeomatryProblems
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine(" ====== UC-1 output ======\n");
+            Test obj;
+            obj = new Uc1();
+            Console.WriteLine(" ====== UC-1 output ======\n");
+            obj.Length();
+
+            Console.WriteLine(" ====== UC-2 output ======\n");
+            obj = new Uc2();
+            obj.Length();
+
+            Console.WriteLine(" ====== UC-3 output ======\n");
+            obj = new Uc3();
+            obj.Length();
+        }
+    }
+}
