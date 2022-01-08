d5ba9ac58132f585f19ca95f5583919572d57d32
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-08 13:12:07 +0530

    Adding Test.cs File

---
+++ Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_GeomatryProblems
+{
+    public abstract class Test
+    {
+        public static int xLength=0, yLength=0;
+         static Test()
+       // static
+        {
+            int x1, x2, y1, y2;
+            Console.WriteLine("Enter X1 value :-");
+            x1 =Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter X2 value :-");
+            x2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Y1 value :-");
+            y1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Y2 value :-");
+            y2 = Int32.Parse(Console.ReadLine());
+
+            xLength = (x1 - x2) * (x1 - x2);
+            yLength = (y1 - y2) * (y1 - y2);
+
+        }
+        public abstract void Length();
+    }
+}
